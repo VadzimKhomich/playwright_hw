e9bace142ff6bdffbba490f6f67dcce1b2a84bee
Vadzim Khomich <vadzim.khomich@activeplatform.com>
2025-05-09 19:04:37 +0300

    HW6 Playwrigh

---
+++ src/config/api.config.ts
@@ -0,0 +1,9 @@
+export const apiConfig = {
+    BASE_URL: "https://aqa-course-project.app",
+    ENDPOINTS: {
+        CUSTOMERS: "/api/customers",
+        CUSTOMER_BY_ID: (id: string) => `/api/customers/${id}`,
+        LOGIN: "/api/login",
+        PRODUCTS: "/api/products"
+    }
+} as const
---
+++ src/data/shemas/login/login.shema.ts
@@ -0,0 +1,44 @@
+import { ROLES } from "data/customers/roles.data";
+
+export const loginChema = {
+  type: "object",
+  properties: {
+    IsSuccess: {
+      type: "boolean",
+    },
+    ErrorMessage: { type: ["string", "null"] },
+    User: {
+      type: "object",
+      properties: {
+        _id: {
+          type: "string",
+        },
+        username: {
+          type: "string",
+        },
+        firstName: {
+          type: "string",
+        },
+        lastName: {
+          type: "string",
+        },
+        roles: {
+          type: "array",
+          items: { type: "string", enum: Object.values(ROLES) },
+        },
+        createdOn: {
+          type: "string",
+        },
+      },
+      required: [
+        "_id",
+        "username",
+        "firstName",
+        "lastName",
+        "roles",
+        "createdOn",
+      ],
+    },
+  },
+  required: ["IsSuccess", "ErrorMessage", "User"],
+};
